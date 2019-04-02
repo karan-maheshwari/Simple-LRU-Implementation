d33e541a469210b3662af81eedd29cd85004de43
karan-maheshwari <karanmaheshwari24@gmail.com>
2019-04-02 12:46:27 -0700

    Changed version
    Changed version

--- MySimpleLRUCache_KaranMaheshwari/setup.py
+++ MySimpleLRUCache_KaranMaheshwari/setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'MySimpleLRUCache_KaranMaheshwari',
   packages = ['MySimpleLRUCache_KaranMaheshwari'],
-  version = '0.4',
+  version = '0.5',
   license='MIT',
   description = 'A simple implementation of LRU Caching algorithm.',
   author = 'Karan Maheshwari',
   author_email = 'karanmaheshwari24@gmail.com',
   url = 'https://github.com/karan-maheshwari',
-  download_url = 'https://github.com/karan-maheshwari/Simple-LRU-Implementation/archive/v_01.tar.gz',
+  download_url = 'https://github.com/karan-maheshwari/Simple-LRU-Implementation/archive/v_04.tar.gz',
   keywords = ['lru', 'cache', 'caching'],
   install_requires=[],
   classifiers=[
