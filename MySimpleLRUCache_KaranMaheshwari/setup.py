756d67c0a75959990029d805263af999a24b2c52
karan-maheshwari <karanmaheshwari24@gmail.com>
2019-04-02 12:03:37 -0700

    Updated file names.
    Updated file names.

--- MySimpleLRUCache_KaranMaheshwari/setup.py
+++ MySimpleLRUCache_KaranMaheshwari/setup.py
@@ -1,20 +1,20 @@
 from distutils.core import setup
 
 setup(
-  name = 'lru_cache',
-  packages = ['lru_cache'],
+  name = 'MySimpleLRUCache_KaranMaheshwari',
+  packages = ['MySimpleLRUCache_KaranMaheshwari'],
   version = '0.1',
   license='MIT',
   description = 'A simple implementation of LRU Caching algorithm.',
   author = 'Karan Maheshwari',
-  author_email = 'kdmahesh@usc.edu',
+  author_email = 'karanmaheshwari24@gmail.com',
   url = 'https://github.com/karan-maheshwari',
   download_url = 'https://github.com/karan-maheshwari/Simple-LRU-Implementation/archive/v_01.tar.gz',
   keywords = ['lru', 'cache', 'caching'],
   install_requires=[],
   classifiers=[
     'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3',
